c7b98c7a3f46aeb8b5c25e980d47f8e84e8e2344
김동일 <ehddlfwkd32@naver.com>
2024-03-05 11:45:54 +0900

    [fix] css

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,17 +1,24 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom"
-
-function Movie({ coverImg, title, summary, genres, id }) {
-    return <div>
-        <img src={coverImg} alt={title} />
-        <h2>
-            <Link to={`/movie/${id}`}>{title}</Link>
-        </h2>
-        <p>{summary}</p>
-        <ul>
-            {genres.map((g) => <li key={g}>{g}</li>)}
-        </ul>
-    </div>;
+import styles from "./Movie.module.css";
+function Movie({ coverImg, title, year, summary, genres, id }) {
+    return (
+        <div className={styles.movie}>
+            <img src={coverImg} alt={title} className={styles.movie__img} />
+            <div>
+                <h2 className={styles.movie__title}>
+                    <Link to={`/movie/${id}`}>{title}</Link>
+                </h2>
+                <h3 className={styles.movie__year}>{year}</h3>
+                <p className={styles.movie__summary}>{summary.length > 0 ? summary : "There is no Commentary..."}</p>
+                <ul className={styles.movie__genres}>
+                    {genres.map((g) => (
+                        <li key={g}>{g}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 Movie.propTypes = {
